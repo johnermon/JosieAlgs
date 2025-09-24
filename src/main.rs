f95e320e71164243cd27992b7eb63b4478dc0a66
Josie Krische <josiekrische@Mac.lan>
2025-09-24 09:45:06 -0400

    added valid parentheses algorithm

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,35 @@ fn main() {
     sum_indexes(10, &input);
     let input: &'static str = "aloikjhjasdfyhui iouehjfgopwq;ewrdfgjwqdp[ovmwpeoirkjfvcwq;olkdjvsa;dlqwertyuiopaSDFGHJKLkgjkfs[v[vqerpokrvfg";
     longest_nonrepeating_substring(input);
+    valid_parentheses("{(])}");
+}
+
+fn valid_parentheses(input: &'static str) {
+    let mut stack: Vec<char> = Vec::with_capacity(input.len());
+    'outer: for curr_char in input.chars() {
+        if curr_char == '(' || curr_char == '{' || curr_char == '[' {
+            stack.push(curr_char);
+            continue;
+        }
+        let open_bracets = match curr_char {
+            ')' => '(',
+            '}' => '{',
+            ']' => '[',
+            _ => continue,
+        };
+        for i in (0..stack.len()).rev() {
+            if open_bracets == stack[i] {
+                let _ = stack.remove(i);
+                continue 'outer;
+            }
+        }
+        break 'outer;
+    }
+    if stack.last().is_none() {
+        println!("Valid Input");
+    } else {
+        println!("Invalid Input");
+    }
 }
 
 //finds the index of a sum that equals to
@@ -36,7 +65,7 @@ fn longest_nonrepeating_substring(input: &'static str) {
     //the range in the string where the longest substring lives
     let mut longest_substring_index: Range<usize> = 0..0;
     //curr substring is the index of the current substring loaded in memory
-    let mut curr_substring: usize = 0
+    let mut curr_substring: usize = 0;
     //array containing optional instances of the last index where each character was seen
     let mut char_indexes: [Option<usize>; 128] = [None; 128];
     //iterates from zero until the length of the input string
@@ -46,7 +75,9 @@ fn longest_nonrepeating_substring(input: &'static str) {
         //char index is a mutable reference to the index in the array where the current ascii character is located at
         let char_index = &mut char_indexes[index];
         //checks if the character currently exists in the charindexes index, and if that character is at a greater than or equal index to the curr substring index
-        if let Some(char) = *char_index && char >= curr_substring {
+        if let Some(char) = *char_index
+            && char >= curr_substring
+        {
             //checks if the current substring is longer than the previous max substring. if so sets max substring to be equal to current substring
             if i - curr_substring > longest_substring_index.end - longest_substring_index.start {
                 longest_substring_index = curr_substring..i;
