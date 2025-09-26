c2412515e3b0dd65a404cba7e1f4a46d9b1cd4da
Josie <36011912+johnermon@users.noreply.github.com>
2025-09-25 23:10:53 -0400

    longest_nonrepeating_substring.hpp を更新
    Removed unnecessary inline std declaration

--- cpp/src/longest_nonrepeating_substring.hpp
+++ cpp/src/longest_nonrepeating_substring.hpp
@@ -18,7 +18,7 @@ using std::tuple;
 inline string_view longest_nonrepeating_substring(string_view input) {
   tuple<size_t, size_t> longest_substring_range;
   size_t curr_substring_index = 0;
-  array<std::optional<size_t>, 128> char_indexes;
+  array<optional<size_t>, 128> char_indexes;
   char_indexes.fill(nullopt);
   for (size_t i = 0; i < input.size(); i++) {
     char curr = input[i];
