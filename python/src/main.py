398a902968ec74da95f0197a201be1c39d10f57f
Josie Krische <josiekrische@mac.mynetworksettings.com>
2025-10-01 10:43:04 -0400

    ported intersecting brackets to python

--- python/src/main.py
+++ python/src/main.py
@@ -1,6 +1,13 @@
 from .longest_nonrepeating_substring import longest_nonrepeating_substring
+from .valid_intersecting_brackets import valid_intersecting_brackets
 
 def main():
     input = "aloikjhjasdfyhuiiouehjfgopwq;ewrdfgjwqdp[ovmwpeoirkjfvcwq;olkdjvsa;dlqwertyuiopaSDFGHJKLkgjkfs[v[vqerpokrvfg";
     output = longest_nonrepeating_substring(input)
     print(f"longest substring is: \n{output}\n")
+    strings = ["{{}<[>]}", "{{", "]]", "<<{>{[}>(}])"]
+    for string in strings:
+        if valid_intersecting_brackets(string):
+            print(string, "is a valid input")
+        else:
+            print(string, "is an invalid input")
---
+++ python/src/valid_intersecting_brackets.py
@@ -0,0 +1,40 @@
+def valid_intersecting_brackets(input:str) -> bool:
+    stack:list[str] = []
+    for char in input:
+        if char in  ('(' , '{', '[', '<'):
+            stack.append(char)
+            continue
+        
+        #im glad after 20 years python made match cases, thats cool ig
+        open:str
+        match char:
+            case ')':
+                open = '('
+            case '}':
+                open = '{'
+            case ']':
+                open = '['
+            case '>':
+                open = '<'
+            case _:
+                continue
+
+        countinue_outer = False
+        for i, char in enumerate(stack):
+            if open == char:
+                countinue_outer = True
+                stack.pop(i)
+                break
+
+        #to those who say that a flag and a branch are more ideomatic in this case than a single
+        #bracket, i say no you are wrong srry, unstructured control flow works better here
+        if countinue_outer:
+            continue
+
+        return False
+
+    #the idea of "truthy" values hurts my head
+    return not bool(stack)
+
+
+                
